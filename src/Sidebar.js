2192e4f921b97ae128dc1d8866400efffafb1fc1
Tirth Patel <177tpatel@gmail.com>
2021-05-08 01:24:53 +0530

    Starter

---
+++ src/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Sidebar.css'
+import { Avatar } from 'material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+
+/**
+* @author Tirth Patel
+* @function Sidebar
+**/
+
+const Sidebar = (props) => {
+  return(
+    <div className='sidebar'>
+        <div className='sidebar_header'>
+            <Avatar />
+            <div className='sidebar_headerRight'>
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
+            </div>
+        </div>
+ 
+        <div className='sidebar_search'>
+
+        </div>
+
+        <div className='sidebar_chats'>
+
+        </div>
+    </div>
+   )
+
+ }
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXCKO5ggIN4r0EVfzoJHnAstbMusgGuR0",
+    authDomain: "whatsapp-clone-7f8ad.firebaseapp.com",
+    projectId: "whatsapp-clone-7f8ad",
+    storageBucket: "whatsapp-clone-7f8ad.appspot.com",
+    messagingSenderId: "675736414037",
+    appId: "1:675736414037:web:fdb318c77e2a2d1f5fb1dc",
+    measurementId: "G-TXFD3RG3X1"
+  };
