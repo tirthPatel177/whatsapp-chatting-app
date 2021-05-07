ca21764618e4639a9ebbf1329853d1d59a9cd583
Tirth Patel <177tpatel@gmail.com>
2021-05-08 02:06:15 +0530

    Resolved Babel Dependency Issue

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Sidebar.css'
-import { Avatar } from 'material-ui/core'
+import { Avatar } from "@material-ui/core"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
